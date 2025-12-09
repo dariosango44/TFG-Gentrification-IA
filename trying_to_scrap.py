43bd4db23ee5304362365aea61a221d8f2811ba1
dariosanxe <dariosango44@gmail.com>
2025-12-09 16:22:54 +0100

    I have created the file to start the scrapping activity

---
+++ trying_to_scrap.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  9 15:34:29 2025
+
+@author: dariosango44
+@description: le voy dando caña a la vaina bacana del tfg de info
+
+"""
+
